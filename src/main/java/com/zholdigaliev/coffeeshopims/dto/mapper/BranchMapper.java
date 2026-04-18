de450370ee0522b1fbcb5e8e62410b6ececa13fb
siko-zholdilgaiev <sikozholdigaliev@gmail.com>
2026-04-18 23:31:58 +0500

    fix: Fix mistakes BranchMapper

--- src/main/java/com/zholdigaliev/coffeeshopims/dto/mapper/BranchMapper.java
+++ src/main/java/com/zholdigaliev/coffeeshopims/dto/mapper/BranchMapper.java
@@ -1,13 +1,13 @@
 package com.zholdigaliev.coffeeshopims.dto.mapper;
 
-import com.zholdigaliev.coffeeshopims.dto.BranchDto.BranchRequest;
-import com.zholdigaliev.coffeeshopims.dto.BranchDto.BranchResponse;
+import com.zholdigaliev.coffeeshopims.dto.BranchDto.ProductRequest;
+import com.zholdigaliev.coffeeshopims.dto.BranchDto.ProductResponse;
 import com.zholdigaliev.coffeeshopims.entity.Branch;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface BranchMapper {
-    BranchResponse toResponse(Branch branch);
+    ProductResponse toResponse(Branch branch);
 
-    Branch toEntity(BranchRequest request);
+    Branch toEntity(ProductRequest request);
 }
